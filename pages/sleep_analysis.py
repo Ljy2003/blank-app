c442df165ae7d83d11226c2e2ce410a24c443966
Ljy2003 <99071185+Ljy2003@users.noreply.github.com>
2024-07-25 15:42:52 +0800

    Update sleep_analysis.py

--- pages/sleep_analysis.py
+++ pages/sleep_analysis.py
@@ -20,7 +20,7 @@ stage = ['Wake','REM','N1','N2','N3']
 fig = plt.figure(figsize=[10,5])
 ax = fig.add_subplot(1,1,1)
 ax.plot(stages)
-st.markdown('## 睡眠分期结果')
+st.markdown('### 睡眠分期结果')
 ax.set_yticks([4,3,2,1,0],stage)
 st.pyplot(fig)
 
@@ -29,7 +29,7 @@ with col1:
     fig = plt.figure(figsize=[5,5])
     ax = fig.add_subplot(1,1,1)
     ax.pie([np.sum(stages==4),np.sum(stages==3),np.sum(stages==2),np.sum(stages==1),np.sum(stages==0)],labels=stage)
-    st.markdown('## 睡眠阶段占比')
+    st.markdown('### 睡眠阶段占比')
     st.pyplot(fig)
 with col2:
     st.markdown('觉醒次数：4')
@@ -38,7 +38,7 @@ with col2:
     st.markdown('REM潜伏期：{}分钟'.format(np.where(stages==3)[0][0]/2))
 st.markdown("## 睡眠建议")
 st.markdown('''
-- ### 中途觉醒次数过多，注意睡前不要饮酒或喝咖啡。
-- ### REM潜伏期过长，需要减少白天酒精摄入量。
-- ### 深度睡眠时间较少，平时加强锻炼，控制体重。
+- 中途觉醒次数过多，注意睡前不要饮酒或喝咖啡。
+- REM潜伏期过长，需要减少白天酒精摄入量。
+- 深度睡眠时间较少，平时加强锻炼，控制体重。
 ''')
